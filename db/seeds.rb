03f31eba869cd0e967537eaac21763287225c787
Genemichael25 <genemichael25@yahoo.com>
2022-08-17 14:13:04 -0700

    Finished seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.where(email: 'test@test.com').first_or_create(password: '12345678', password_confirmation: '12345678')
+user2 = User.where( email: 'user2@test.com').first_or_create(password: '87654321', password_confirmation: '87654321')
+
+ 
+trips1 = [
+  {
+    name: 'Grand Canyon',
+    location: 'Arizona',
+    from: 20220703,
+    to: 20220705,
+    travel_buddies: 'Code Empire Team',
+    comments: 'Had a blast with the team after launching our app.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Dawn_on_the_S_rim_of_the_Grand_Canyon_%288645178272%29.jpg'
+  },
+  {
+    name: 'Petra',
+    location: 'Jordan',
+    from: 20220510,
+    to: 20220517,
+    travel_buddies: 'Friends from High School',
+    comments: 'Instead of a reunion we decided to travel with our close group of friends',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc2yzt4nT1SjYbV3itXx6HZHVmqpCrsRsBbQ&usqp=CAU'
+  }
+]
+  
+trips2 = [
+  {
+    name: 'Las Vegas Strip',
+    location: 'Las Vegas, Nevada',
+    from: 20220305,
+    to: 20220310,
+    travel_buddies: 'Spouse',
+    comments: 'Quick getaway with my significant other',
+    image: 'https://live.staticflickr.com/82/245235200_2c753b3671_b.jpg'
+  }
+]
+
+trips1.each do |each_trip|
+  user1.trips.create each_trip
+  puts "creating trip #{each_trip}"
+end
+
+trips2.each do |each_trip|
+  user2.trips.create each_trip
+  puts "creating trip #{each_trip}"
+end
