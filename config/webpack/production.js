86ba059bbc79b143f64258cb90b73839d32387ef
LeoContreras93 <Contreras.leo2211@gmail.com>
2022-08-23 11:38:51 -0700

    changes #2

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 const ignoreTestFiles = {
-    test: /test\.js$/
+    test: /test\.js$/,
+    loader: 'ignore-loader'
   }
   environment.loaders.append('ignoreTestFiles', ignoreTestFiles)
 module.exports = environment.toWebpackConfig()
