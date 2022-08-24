5a3c124ff70f3313d9a0c1b7ea8ff6c62163550c
Genemichael25 <genemichael25@yahoo.com>
2022-08-24 11:37:11 -0700

    Not Found styling complete

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -5,8 +5,20 @@ class NotFound extends Component {
   render() {
     return (
     <>
-    <h3> OOPS, PAGE NOT FOUND!</h3> 
-    
+      <div className="notfound">
+        <img
+          className="notfound-image"
+          src="assets/desert2.jpeg"
+          alt="notfound"
+          width={"100%"}
+          height={"650px"}
+        >
+        </img>
+        <div className="centered">
+          <h3> 404</h3> 
+          <p>Oops, your destination is out of this world...</p>
+        </div>
+      </div>
     </>
     )
   }
