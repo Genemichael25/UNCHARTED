ed2d26ed71f830fca8a724b5e50eef61ba44d718
Genemichael25 <genemichael25@yahoo.com>
2022-08-25 10:57:17 -0700

    Styled show pages, DRY Seven Wonders page

--- app/javascript/components/pages/TripShow.js
+++ app/javascript/components/pages/TripShow.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import {
   Card,
@@ -11,41 +10,51 @@ import {
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
-
-
 class TripShow extends Component {
-
   render() {
-    let { trip } = this.props;
+    let { trip } = this.props
     return (
       <>
         <div>
-          {trip && 
-          <Card>
-            <CardImg className="trip-image" src={trip.image} alt="trip card image" />
-            <CardBody className="trip-detail-body">
-
-              <CardSubtitle>{trip.name}</CardSubtitle>
-              <CardTitle>{trip.location}</CardTitle>
-              <br />
-              <CardText>Dates: {trip.from} to {trip.to}</CardText>
-              <CardText>Travel Buddies: {trip.travel_buddies}</CardText>
-              <CardText>{trip.comments}</CardText>
-              <NavLink to={`/tripedit/${trip.id}`}>
-                <Button>Update trip</Button>
-              </NavLink>
-              <NavLink to={`/tripindex`}>
-              <Button onClick={() => this.props.deleteTrip(trip.id)}>
-                Delete Trip
-              </Button>
-              </NavLink>
-            </CardBody>
-          </Card>
-          }
+          {trip && (
+            <Card className="showcard">
+              <CardImg
+                className="trip-image"
+                src={trip.image}
+                alt="trip card image"
+              />
+              <CardBody className="trip-detail-body">
+                <CardTitle className="nameofplace">{trip.name}</CardTitle>
+                <CardSubtitle>{trip.location}</CardSubtitle>
+                <hr></hr>
+                <CardText>
+                  Dates: {trip.from} to {trip.to}
+                </CardText>
+                <CardText>
+                  Travel Buddies: {trip.travel_buddies}
+                </CardText>
+                <CardText>{trip.comments}</CardText>
+                <NavLink to={`/tripedit/${trip.id}`}>
+                  <Button 
+                    style={{ marginLeft: "15%" }}>
+                      Edit Trip
+                  </Button>
+                </NavLink>
+                <NavLink to={`/tripindex`}>
+                  <Button
+                    onClick={() => this.props.deleteTrip(trip.id)}
+                    style={{ marginLeft: "20%" }}
+                  >
+                    Delete Trip
+                  </Button>
+                </NavLink>
+              </CardBody>
+            </Card>
+          )}
         </div>
       </>
     )
   }
 }
 
-export default TripShow;
+export default TripShow
