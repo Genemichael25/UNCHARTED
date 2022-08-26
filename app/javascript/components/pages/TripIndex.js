da443ce604fa065925479b8fa888a948620b9e0f
scharlez <stevencharlez@gmail.com>
2022-08-26 14:18:25 -0700

    Cleaned up identation

--- app/javascript/components/pages/TripIndex.js
+++ app/javascript/components/pages/TripIndex.js
@@ -6,62 +6,63 @@ class TripIndex extends Component {
 render() {
   return (
   <>
-  <div class="body"> 
-    <Container className="indexText">
-      <h3 id="index-title-text">
-        My Adventures
-      </h3>
-      <p id="index-subtitle-text">
-        Everywhere I've been...
-      </p>
-    </Container>
-    <Row className="trips-row">
-      {this.props.trips &&
-        this.props.trips.map((trip) => {
-          return (
-            <Col 
-            key={trip.id} 
-            sm="4" 
-            className="col-lg-4 d-flex align-items-stretch"
-            >
-              <Card className="trip-title" body>
-                <CardImg 
-                  id="index-card-images"
-                  top
-                  width="100%"
-                  src={trip.image}
-                  alt="Card image cap"
-                />
-                <CardTitle className="trip-basic-info">
-                  <div className="nameofplace">
-                  {trip.name}
-                  <br></br>
-                  </div>
-                  <div className="location-text">
-                  {trip.location}
-                  </div>
-                  <div className="date-text"> 
-                  <hr></hr>
-                    Date: {trip.from} to {trip.to}
-                  </div>
-                  <br></br>
-                  <p>
-                    <div class="col-md-12 text-center"> 
-                    <NavLink 
-                      to={`/tripshow/${trip.id}`}>
-                      <Button href="#" className="s-button">More Details</Button>
-                    </NavLink>
-                    </div>
-                  </p>
-                </CardTitle>
-              </Card>
-            </Col>
-          )
-        })}
-    </Row>
-    </div>
-  </>
-)
-}
+    <div class="body"> 
+      <Container className="indexText">
+        <h3 id="index-title-text">
+          My Adventures
+        </h3>
+        <p id="index-subtitle-text">
+          Everywhere I've been...
+        </p>
+      </Container>
+        <Row className="trips-row">
+          {this.props.trips &&
+            this.props.trips.map((trip) => {
+              return (
+                  <Col 
+                    key={trip.id} 
+                    sm="4" 
+                    className="col-lg-4 d-flex align-items-stretch">
+                  <Card className="trip-title" body>
+                    <CardImg 
+                      id="index-card-images"
+                      top
+                      width="100%"
+                      src={trip.image}
+                      alt="Card image cap"
+                    />
+                    <CardTitle className="trip-basic-info">
+                      <div className="nameofplace">
+                        {trip.name}
+                      <br></br>
+                      </div>
+                      <div className="location-text">
+                        {trip.location}
+                      </div>
+                      <div className="date-text"> 
+                      <hr></hr>
+                        Date: {trip.from} to {trip.to}
+                      </div>
+                      <br></br>
+                      <p>
+                        <div class="col-md-12 text-center"> 
+                        <NavLink 
+                            to={`/tripshow/${trip.id}`}>
+                          <Button href="#" className="s-button">
+                            More Details
+                          </Button>
+                        </NavLink>
+                        </div>
+                      </p>
+                    </CardTitle>
+                  </Card>
+                </Col>
+              )
+            })}
+          </Row>
+      </div>
+    </>
+    )
+  }
 }
 export default TripIndex;
